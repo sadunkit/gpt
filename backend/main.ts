acd1f956c87c76a95e03d82515de6bdb726236d5
sadunkit <sadun@kityazilim.com>
2024-03-15 12:00:05 +0300

    added sockets

--- backend/main.ts
+++ backend/main.ts
@@ -1,8 +1,32 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = new Application();
+const router = new Router();
+
+router.get("/wss", (ctx) => {
+  if (!ctx.isUpgradable) {
+    ctx.throw(501);
+  }
+  const ws = ctx.upgrade();
+
+  ws.onopen = () => {
+    console.log("Connected to client");
+    ws.send("Hello from server!");
+  };
+
+  ws.onmessage = (m) => {
+	if (m.data === "ping") {
+		ws.send("pong");
+		return;
+	} else {
+		console.log("Got message from client: ", m.data);
+	}
+  };
+
+  ws.onclose = () => console.log("Disconncted from client");
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen();
--- backend/main_test.ts
+++ backend/main_test.ts
@@ -1,6 +1,28 @@
 import { assertEquals } from "https://deno.land/std@0.216.0/assert/mod.ts";
-import { add } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(async function websocketTest() {
+    const result = await new Promise((resolve) => {
+		const ws = new WebSocket("ws://localhost:4488/wss");
+		ws.onopen = () => {
+			console.log("Connected to server")
+
+			setInterval(() => {
+				ws.send("ping");
+			}, 1000);
+		};
+		ws.onmessage = (m) => {
+			if (m.data === "pong") {
+				console.log("Got pong from server");
+				return;
+			}
+			console.log("Got message from server: ", m.data);
+      		ws.send(`Some message ${crypto.randomUUID()}`);
+		};
+		ws.onclose = () => {
+			console.log("Disconnected from server");
+			resolve(1);
+		}
+	});
+
+	assertEquals(result, 1);
 });
